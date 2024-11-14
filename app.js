106a4fa484605e3d683ffdbcd6237bcb6f96280a
ghanley04 <gghanley04@gmail.com>
2024-11-13 22:10:55 -0500

    updates

--- app.js
+++ app.js
@@ -27,6 +27,8 @@ function update(cursorPosition) {
 update(0);
 
 window.addEventListener("mousemove", (e) => {
+    if(timeline.isActive()) return;
+
     xValue = e.clientX - window.innerWidth / 2;
     yValue = e.clientY - window.innerHeight / 2;
 
@@ -47,13 +49,42 @@ window.addEventListener("mousemove", (e) => {
 
 let timeline = gsap.timeline();
 
-parallax_el.forEach((el) => {
+Array.from(parallax_el)
+    .filter(el => !el.classList.contains("text"))
+    .forEach(el => {
     timeline.from(
         el,
         {
-            top: `${el.offsetHeight / 2 - el.dataset.distance}px`,
-            duration: 1,
+            top: `${el.offsetHeight / 2 + +el.dataset.distance}px`,
+            duration: 3.5,
+            ease: "power3.out",
         },
         "1"
     );
 });
+
+timeline.from(".text h1", {
+    y: 
+        window.innerHeight - 
+        document.querySelector(".text h1").getBoundingClientRect().top +
+        200,
+    duration: 2,
+    },
+    "2.5"
+).from(
+    ".text h2", 
+    {
+        y: -150,
+        opacity: 0,
+        duration: 1.5,
+    },
+    "3"
+)
+.from(
+    ".hide",
+    {
+        opacity: 0,
+        duration: 1.5,
+    }, 
+    "3"
+);
